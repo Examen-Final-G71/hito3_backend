82ea4b1671e482f84e996321e5968c70d24c9627
Fleepx <felipesanchez.aq@gmail.com>
2025-02-20 00:10:38 -0300

    config

--- config.js
+++ config.js
@@ -3,11 +3,7 @@ dotenv.config();
 
 const envs = {
   PORT: process.env.PORT || 3000,
-  DB_PORT: process.env.PORT || 5432,
-  DB_USER: process.env.USER_DB,
-  DB_HOST: process.env.HOST_DB,
-  DB_NAME: process.env.DATABASE,
-  DB_PASSWORD: process.env.PASSWORD_DB,
+  DATABASE_URL: process.env.DATABASE_URL,
 };
 
 module.exports = {  PORT,  USER_DB,  HOST_DB,  DATABASE,  PASSWORD_DB } = envs;
