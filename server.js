5a6d07a0d3a904cc2dff2b68666339064cc7b040
Fleepx <felipesanchez.aq@gmail.com>
2025-02-20 22:55:08 -0300

    pool

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const express = require('express');
 const cors = require('cors');
 const pool = require('./connection');
 
+pool.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.error('Error conectando a la base de datos:', err);
+  } else {
+    console.log('Conexión a la base de datos exitosa:', res.rows[0]);
+  }
+});
+
 const authRoutes = require('./routes/authRoutes');
 const usuarioRoutes = require('./routes/usuarioRoutes');
 const publicacionRoutes = require('./routes/publicacionRoutes');
@@ -10,16 +18,20 @@ const comentarioRoutes = require('./routes/comentarioRoutes');
 const transaccionRoutes = require('./routes/transaccionRoutes'); 
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
 app.use(cors({
   origin: 'https://hito2-frontend.onrender.com',
   methods: 'GET,POST,PUT,DELETE',
   allowedHeaders: 'Content-Type,Authorization'
 }));
 
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Servidor funcionando correctamente");
 });
+
 app.use('/api/auth', authRoutes);
 app.use('/usuarios', usuarioRoutes);
 app.use('/publicaciones', publicacionRoutes);
