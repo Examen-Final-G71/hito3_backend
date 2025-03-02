304b2a021339f858a7065aa8d6c1952ab3213ae6
Fleepx <felipesanchez.aq@gmail.com>
2025-03-02 14:01:08 -0300

    Comentarios

--- routes/comentarioRoutes.js
+++ routes/comentarioRoutes.js
@@ -4,8 +4,8 @@ const authenticateToken = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
-router.get('/:publicacion_id', obtenerComentarios);
-router.post('/:publicacion_id', authenticateToken, crearComentario);
+router.get('/publicacion/:publicacion_id/comentarios', obtenerComentarios);
+router.post('/publicacion/:publicacion_id/comentarios', authenticateToken, crearComentario);
 router.delete('/:id', authenticateToken, eliminarComentario);
 
 module.exports = router;
