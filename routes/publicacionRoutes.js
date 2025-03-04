516e92287089354701bb20c3b7a5f6f168e10012
Fleepx <felipesanchez.aq@gmail.com>
2025-03-03 22:45:46 -0300

    PublicacionesById

--- routes/publicacionRoutes.js
+++ routes/publicacionRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { obtenerPublicaciones, crearPublicacion, eliminarPublicacion, editarPublicacion } = require('../controllers/publicacionController');
+const { obtenerPublicaciones, crearPublicacion, eliminarPublicacion, editarPublicacion, obtenerPublicacionesPorUsario } = require('../controllers/publicacionController');
 const authenticateToken = require('../middleware/authMiddleware');
 const upload = require('../middleware/uploadMiddleware');
 
 const router = express.Router();
 
 router.get('/', obtenerPublicaciones);
+router.get('/:id', authenticateToken, obtenerPublicacionesPorUsario);
 router.post('/', authenticateToken, upload.single('imagen'), crearPublicacion);
 router.put('/:id', authenticateToken, editarPublicacion);
 router.delete('/:id', authenticateToken, eliminarPublicacion); 
